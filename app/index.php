938c564e4b763aeb2023f51a7b5bbc3c812dd5c8
Mario Paricio <68949370+MarioParicio@users.noreply.github.com>
2024-03-17 17:45:20 +0100

    Initial commit

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Página de Inicio</title>
+</head>
+<body>
+<h1>Bienvenido a mi sitio web</h1>
+<p>Selecciona una página para visitar:</p>
+<ul>
+    <li><a href="pagina1.php">Página 1</a></li>
+
+</ul>
+</body>
+</html>
---
+++ app/pagina1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pagina 1</title>
+</head>
+<body>
+<h1>Pagina 1</h1>
+<p>Pagina 1</p>
+
+</body>
+</html>
